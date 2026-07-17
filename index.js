3e61290fdf0989c0f066087421ab23d53bf0cf37
Rajal Suwal <suwalrajal57@gmail.com>
2026-07-17 20:51:18 +0900

    closure

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-Array.prototype.newArray = function (callback) {
-  const newArr = [];
+//closure
+//magine you have a magic backpack.Inside this backpack, you put a notebook with a secret password written on it.Even if you walk away from your bedroom and go to a completely different school, whenever you open that specific backpack, you can still read your secret password.In JavaScript, a closure is exactly like that magic backpack. It allows a function to remember and access variables from its original home, even after that home has closed down
 
-  for (let i = 0; i < this.length; i++) {
-    //check if value is not empty
-    if (this.hasOwnProperty(i)) {
-      const storedArr = callback(this[i], i, this);
-        console.log(storedArr)
+function bagPack(){
+    let secret = "this is a scret text";
 
-      //pushiing the value to new arr
-      newArr.push(storedArr);
+    return  function(){  //this function has the acess to the outer scopes
+        console.log(secret)
     }
-  }
 
-  return newArr;
-};
 
-const number = [1, 2, 3];
-const double = number.newArray((num) => num * 2);
-console.log(double)
+}
+bagPack()();//this is a scret text
+
+const doraimonBag = bagPack();
+doraimonBag() //this is a scret text
+
+console.log(doraimonBag) //[Function (anonymous)] // this is a function ready to be called 
+
+
+
+
+//EXPLANTAION
+// CLOSURES bagPack function {A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives a function access to its outer scope. In JavaScript, closures are created every time a function is created, at function creation time.}
