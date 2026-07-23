2f5c0f272c4ad3ca278325ae82725751e302ef49
Rajal7777 <suwalrajal57@gmail.com>
2026-07-23 10:41:35 +0900

    count the char in the string

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
-const orignalUser = {
-  name: "Alex",
-  metadata: { role: "admin" },
-};
+/*
 
-const copyUser = { ...orignalUser };  
-//console.log("original", orignalUser);  //original { name: 'Alex', metadata: { role: 'admin' } }
-//console.log("copy", copyUser);  //copy { name: 'Alex', metadata: { role: 'admin' } }
+const str = 'apple';
+function countChr(str){
+  const obj= {};
 
+  for(const char of str){
+    console.log('char', char)  // a p p l e
+    obj[char] = (obj[char] || 0) + 1;
+    console.log(obj[char])  //1 1 2 1 1 
+  }
 
-//copyUser.name = 'Rajal';
+  return obj;
+}
 
-//console.log(copyUser);  //{ name: 'Rajal', metadata: { role: 'admin' } }
-//console.log('original',orignalUser) //original { name: 'Alex', metadata: { role: 'admin' } }
+const res = countChr(str);
 
+console.log(res);  //{ a: 1, p: 2, l: 1, e: 1 }
+ // || operator if the value in the left is falsy then use right value
 
-//change the deep nested obj
-//will cahnge the original oobject as well so it is dangerous.
+ //obj obj[char] -> obj(["a"]  that does not exist on the obj so it returns 0 then 0 + 1  becomes 1)
+ //obj(["a"] = 1)  => { a: 1} 
 
-//copyUser.metadata.role = 'user';
+const obj = {
+  name: 'Rajal',
+}
 
-//console.log('original', orignalUser)   //original { name: 'Alex', metadata: { role: 'user' } }
-
-
-//it is safe to use will not affect the original obj
-const deepCopy = structuredClone(orignalUser);
-
-//console.log(deepCopy);   //{ name: 'Alex', metadata: { role: 'admin' } }
-deepCopy.metadata.role ='user';
-console.log(orignalUser)  //{ name: 'Alex', metadata: { role: 'admin' } }
+console.log('from obj', obj["name"]) //Rajal
 
 
-const newCopy = JSON.parse(JSON.stringify(orignalUser));
-newCopy.metadata.role = 'public'
-console.log('NewCopy', newCopy);
-console.log(orignalUser) 
+*/
 
-let  p ={...orignalUser,metadata:{...orignalUser.metadata,role:'public'}};
-console.log('P', p);
+/*
+USING REDUCE METHOD
+const str = 'apple';
+
+function countChar(str){
+  //reduce method can be only use in array so we convert str to array also we can use str.split('') to convert str to array
+  return [...str].reduce((acc,char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});  //here we give the accumulator initial value as empty object
+}
 
 
+const res = countChar(str);
+console.log(res)
 
+*/
+
+const str = "apple";
+
+function countChar(str) {
+  const obj = {};
+
+  for (const char of str) {
+    if (obj[char]) {
+      obj[char] += 1;
+      console.log('if block',char, obj[char], obj);
+    } else {
+      obj[char] = 1;
+      console.log('else block',char, obj[char], obj);
+    }
+  }
+
+  return obj;
+}
+const res = countChar(str);
+
+console.log(res);
+/*
+else block a 1 { a: 1 } 1st loop  -> 1st time obj['a'] is not there so goes to if block
+else block p 1 { a: 1, p: 1 } 2nd
+if block p 2 { a: 1, p: 2 } 3rd  => obj['P'] exist p: 1 -> so stays in if block 1 + 1 -> 2
+else block l 1 { a: 1, p: 2, l: 1 } 4th
+else block e 1 { a: 1, p: 2, l: 1, e: 1 } 5th
+{ a: 1, p: 2, l: 1, e: 1 }
+ */
