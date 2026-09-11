eace3d0efc30958b6f0dae6e4b4f2f1b003ce7da
Rajal7777 <suwalrajal57@gmail.com>
2026-09-11 13:10:50 +0900

    reverse methods

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
+let str = "Rajal";
 
-const numbers = [1, 2, 3, 4, 5];
+function reverseString(str) {
+  let reversed = "";
 
-function rotate(array, k) {
-  const n = array.length;
-  k = k % n;
-  console.log(k)
-  return array.slice(n - k).concat(array.slice(0, n - k));
+  for (let i = str.length -1; i >= 0; i--){
+    reversed += str[i];
+  }
+  return reversed;
 }
+  
 
-console.log(rotate(numbers, 2));
-
-
-/*
-
-Roman Nepali 🧠
-
-Main part:
-
-numbers.slice(-k)
-
-k = 2 bhaye:
-
-[4, 5]
-
-Ani:
-
-numbers.slice(0, -k)
-
-gives:
-
-[1, 2, 3]
-
-Then:
-
-[4, 5].concat([1, 2, 3])
-
-Result:
-
-[4, 5, 1, 2, 3]
-
-*/
+console.log(reverseString(str));
