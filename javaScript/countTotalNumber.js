84ed5816f7b27080bcd05eb998ed3689f5ca647f
Rajal7777 <suwalrajal57@gmail.com>
2026-09-22 20:13:22 +0900

    total sum of the number

--- javaScript/countTotalNumber.js
+++ javaScript/countTotalNumber.js
@@ -2,6 +2,17 @@ let number = 12344;
 
 console.log(countNumber(number));
 
+
+function countNumber(num){
+  let count = 0;
+
+  while(num > 0){
+    count += num %10
+    num = Math.floor(num / 10);
+  }
+  return count;
+}
+
 /*
 time complexity: O(n) where n is the number of digits in the number
 "If n is the number of digits, we convert the number to a string, create an array of its digits, and iterate through all digits using reduce. Each operation is linear, so the overall time complexity is O(n)."
@@ -17,7 +28,3 @@ function countNumber(num) {
 
   return [...number].reduce((acc, num) => acc + Number(num), 0);
 }
-
-let num2 = number.toString();
-console.log(num2);
-console.log([...num2]);
